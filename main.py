c7189a3fea27d1da9e697e922027fea182f36be7
davidluizrusso <david.luiz.russo@gmail.com>
2018-08-01 20:39:26 -0700

    Completed github API exercise

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ import requests
 if __name__ == "__main__":
     username = sys.argv[1]
 
-    # TODO:
-    #
-    # 1. Retrieve a list of "events" associated with the given user name
-    # 2. Print out the time stamp associated with the first event in that list.
+    response = requests.get('https://api.github.com/users/{}/events/public'.format(username))
 
-    print("COMPLETE THE TODOs")
+    print(response.json()[0]['created_at'])
+
+
+
+ 
     
 
 
